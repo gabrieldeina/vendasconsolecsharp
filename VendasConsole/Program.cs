64c94fad7b5ad8620116ef1747d2b6fc7d41c7fb
Gabriel Deina <gabriel5412@hotmail.com>
2020-08-18 12:18:34 -0300

    Adição do Menu

--- VendasConsole/Program.cs
+++ VendasConsole/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Xml;
 
-namespace VendasConsole
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace VendasConsole {
+    class Program {
+        static void Main(string[] args) {
+            string[] menu = { "Sair", "Cadastrar Cliente", "Listar Clientes", "Cadastrar Vendedor", "Listar Vendedores",
+                "Cadastrar Produto", "Listar Produtos", "Registrar Venda", "Listar Vendas", "Listar Vendas por Cliente" };
+
+            int opcao;
+
+            do {
+                Console.Clear();
+                for (int i = 0; i < menu.Length; i++) {
+                    Console.WriteLine(i + " - " + menu[i]);
+                }
+                Console.Write("Escolha uma opção: ");
+                opcao = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                switch (opcao) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (opcao != 0);
         }
     }
 }
