514b56ef6c8d7efcd042395a5121f956cf09257f
Gabriel Deina <gabriel5412@hotmail.com>
2020-08-18 14:42:10 -0300

    Criação das opções 1 e 2

---
+++ VendasConsole/Cliente.cs
@@ -0,0 +1,7 @@
+﻿namespace VendasConsole {
+    class Cliente {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+
+    }
+}
--- VendasConsole/Program.cs
+++ VendasConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Xml;
 
 namespace VendasConsole {
@@ -8,6 +9,9 @@ namespace VendasConsole {
                 "Cadastrar Produto", "Listar Produtos", "Registrar Venda", "Listar Vendas", "Listar Vendas por Cliente" };
 
             int opcao;
+            Cliente c = new Cliente();
+            List<Cliente> clientes = new List<Cliente>();
+            bool clienteEncontrado = false;
 
             do {
                 Console.Clear();
@@ -20,14 +24,40 @@ namespace VendasConsole {
 
                 switch (opcao) {
                     case 1:
+                        c = new Cliente();
+                        Console.WriteLine(" --- CADASTRAR CLIENTE --- ");
+                        Console.Write("Informe o nome do cliente: ");
+                        c.Nome = Console.ReadLine();
+                        Console.Write("Informe o cpf do cliente: ");
+                        c.Cpf = Console.ReadLine();
+
+                        if (clientes.Count == 0) {
+                            clientes.Add(c);
+                        } else {
+                            foreach (Cliente clienteCadastrado in clientes) {
+                                if (clienteCadastrado.Cpf == c.Cpf) {
+                                    Console.WriteLine("Esse cliente já existe");
+                                    clienteEncontrado = true;
+                                }
+                            }
+                            if (!clienteEncontrado) {
+                                clientes.Add(c);
+                            }
+                        }
                         break;
                     case 2:
+                        Console.WriteLine(" --- LISTAR CLIENTES --- ");
+                        for (int i = 0; i < clientes.Count; i++) {
+                            Console.WriteLine(clientes[i].Nome);
+                        }
                         break;
                     case 0:
                         break;
                     default:
                         break;
                 }
+                Console.WriteLine("\nAperte uma tecla qualquer...");
+                Console.ReadKey();
 
             } while (opcao != 0);
         }
